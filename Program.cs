3239e47b1a8eff38b42492b87f5fe7ae08d7f392
Igor Alexeev <ibalexeev@gmail.com>
2024-12-11 16:46:23 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace WebPagesDownloaderApp
+{
+    internal class Program
+    {
+        private static CancellationTokenSource? cancellationTokenSource;
+        static async Task Main()
+        {
+            try
+            {
+                cancellationTokenSource = new CancellationTokenSource();
+                var progress = new Progress<ProgressReport>();
+                var sites = new List<string> { "https://www.bbc.com/", "https://www.google.com/" };
+                var results = await WebPagesDownloader.ParallelDownload(sites, progress, cancellationTokenSource);
+                var directory = @"c:\tmp2\pages\";
+                if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+                for (int i = 0; i < results.Count; i++)
+                {
+                    File.WriteAllText(directory + CreateFileName(results[i].Url), results[i].Data);
+                }
+                Console.WriteLine($"All the web pages are in {directory} folder");
+            }
+            catch (Exception ex)
+            {
+                // Todo: add logging
+            }
+            finally
+            {
+                cancellationTokenSource.Dispose();
+            }
+        }
+        private static string CreateFileName(string url)
+        {
+            var index = url.IndexOf("//");
+            if (index != -1)
+            {
+                url = url.Substring(index + 2);
+            }
+            url = url.Replace("/", "");
+            url += ".html";
+            return url;
+        }
+    }
+}
---
+++ WebPagesDownloader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+namespace WebPagesDownloaderApp
+{
+    public class WebPagesDownloader
+    {
+        public static async Task<List<Website>> ParallelDownload(List<string> sites, IProgress<ProgressReport> progress, CancellationTokenSource cancellationTokenSource)
+        {
+            var list = new List<Website>();
+            var progressReport = new ProgressReport();
+            var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism  = 8, CancellationToken = cancellationTokenSource.Token };
+            await Task.Run(() =>
+            {
+                try
+                {
+                    Parallel.ForEach<string>(sites, parallelOptions, (site) =>
+                    {
+                        var result = DownloadWebPage(site);
+                        list.Add(result);
+                        progressReport.SitesDownloaded = list;
+                        progressReport.PercentageCompleted = (list.Count * 100) / sites.Count;
+                        progress.Report(progressReport);
+                        parallelOptions.CancellationToken.ThrowIfCancellationRequested();
+                    });
+                }
+                catch (OperationCanceledException ex)
+                {
+                    throw ex;
+                }
+            });
+            return list;
+        }
+        private static Website DownloadWebPage(string url)
+        {
+            var client = new WebClient();
+            return new Website { Url = url, Data = client.DownloadString(url) };
+        }
+    }
+    public class Website
+    {
+        public string Url { get; set; }
+        public string Data { get; set; }
+    }
+    public class ProgressReport
+    {
+        public int PercentageCompleted { get; set; }
+        public List<Website> SitesDownloaded { get; set; }
+    }
+}
